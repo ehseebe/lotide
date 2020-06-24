0b904f0eccea519c5ac67a02f1590c916d624e23
Alyssa Beaton <ehseebe@gmail.com>
2020-06-24 17:59:00 +0000

    Counts the occurrence of letters in a string

---
+++ countLetters.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`💯Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`💔Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+//input string
+//output a COUNT of letters in the sentence
+//ex. input: LHL, output: L: 2, H:1
+
+const countLetters = function(str) {
+	let arrOfWords = str.split(' ').join('');
+	let result = {};
+	for (let letter of arrOfWords) {
+		result[letter] = (result[letter] || 0) + 1;
+		//**this block is replaced by ln17!
+		//if (result[letter] === undefined) {
+		//	result[letter] = 1;  
+		//} else {
+		//	result[letter] += 1;
+		//}
+		//console.log("result[letter]: ", result[letter]);
+	}
+	return result;
+}
+
+console.log(countLetters('pineapple is the best fruit'));
