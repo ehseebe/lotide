fbb046a335b58dbf4d0927c5eebb2476f1aac0e7
Alyssa Beaton <ehseebe@gmail.com>
2020-06-29 17:37:38 +0000

    Refactored tests into eqArraysTest.js

--- eqArrays.js
+++ eqArrays.js
@@ -20,9 +20,6 @@ const eqArrays = function(arr1, arr2) {
 	return true;
 }
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true) // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false) // => false
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true) // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false)
+module.exports = assertEqual;
+module.exports = eqArrays;
 
