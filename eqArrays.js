4b8eb5e68ab070aaa1350b2028c6470c20a0be7f
Alyssa Beaton <ehseebe@gmail.com>
2020-06-29 18:48:35 +0000

    Added condition to check undefined

--- eqArrays.js
+++ eqArrays.js
@@ -1,25 +1,20 @@
-//const assertEqual = require('./assertEqual');
-// const assertEqual = function(actual, expected) {
-//   if (actual === expected) {
-//     console.log(`💯Assertion Passed: [${actual}] === [${expected}]`);
-//   } else {
-//     console.log(`💔Assertion Failed: [${actual}] !== [${expected}]`);
-//   }
-// };
 
 const eqArrays = function(arr1, arr2) {
-	if (arr1.length !== arr2.length) {
-		return false;
-	} else {
-		for (let i = 0; i < arr1.length; i++) {
-			if (arr1[i] !== arr2[i]) {
-				return false;
-			} else {
-			}
-		}
-	}
-	return true;
-}
+  if (arr1 === undefined && arr2 === undefined) {
+    return true;
+  } else if (arr1.length !== arr2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      } else {
+        return true;
+      }
+    }
+  }
+  return true;
+};
 
 module.exports = eqArrays;
 
