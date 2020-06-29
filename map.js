60015c2f1bd11a86e686e409f73b9289ec2eb9f1
Alyssa Beaton <ehseebe@gmail.com>
2020-06-29 20:24:26 +0000

    Removed assert functions, added export

--- map.js
+++ map.js
@@ -1,32 +1,3 @@
-const eqArrays = function(arr1, arr2) {
-	if (arr1.length !== arr2.length) {
-		return false;
-	} else {
-		for (let i = 0; i < arr1.length; i++) {
-			if (arr1[i] !== arr2[i]) {
-				return false;
-			} else {
-			}
-		}
-	}
-	return true;
-}
-
-const assertArraysEqual = function(arrA, arrB) {
-	let result = eqArrays(arrA, arrB);
-	if (result === true) {
-		console.log("✅ We've got a match!")
-	} else {
-		console.log("⛔️ No match here")
-	}
-}
-
-const words = ['take', 'it', 'easy', 'stay', 'breezy'];
-
-const print = word => word;
-
-// const results1 = map(words, word => word[0]);
-// console.log(results1);
 
 const map = function(array, callback) {
   const results = [];
@@ -39,6 +10,8 @@ const map = function(array, callback) {
   return results;
 };
 
+module.export = map;
 
-console.log(eqArrays(assertArraysEqual(map(words, print)), ['take', 'it', 'easy', 'stay', 'breezy']));
+
+// console.log(eqArrays(assertArraysEqual(map(words, print)), ['take', 'it', 'easy', 'stay', 'breezy']));
 
