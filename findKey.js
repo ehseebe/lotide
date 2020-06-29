f2cf142fcf814afcda71f1dab299972f7ea5c0e3
Alyssa Beaton <ehseebe@gmail.com>
2020-06-29 20:44:15 +0000

    Removed assert functions, added export

--- findKey.js
+++ findKey.js
@@ -1,21 +1,13 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`💯Assertion Passed: [${actual}] === [${expected}]`);
-  } else {
-    console.log(`💔Assertion Failed: [${actual}] !== [${expected}]`);
-  }
-};
+// const michelinResto = {
+//   "Blue Hill": { stars: 1 },
+//   "Akaleri":   { stars: 3 },
+//   "noma":      { stars: 2 },
+//   "elBulli":   { stars: 3 },
+//   "Ora":       { stars: 2 },
+//   "Akelarre":  { stars: 3 }
+// };
 
-const michelinResto = {
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-};
-
-const michelinStarSearch = (item) => item.stars === 3;
+// const michelinStarSearch = (item) => item.stars === 3;
 
 const findKey = function(input, callback) {
   for (let item in input) { //loop IN object
@@ -25,4 +17,6 @@ const findKey = function(input, callback) {
   } return undefined; //else return undefined
 };
 
-console.log(assertEqual(findKey(michelinResto, michelinStarSearch), "Akaleri"));
+// console.log(assertEqual(findKey(michelinResto, michelinStarSearch), "Akaleri"));
+
+module.exports = findKey;
