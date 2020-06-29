d49c1dec4369a31c186baf597628cdd32229557e
Alyssa Beaton <ehseebe@gmail.com>
2020-06-29 20:26:52 +0000

    Removed assert functions, added export

--- flatten.js
+++ flatten.js
@@ -1,28 +1,4 @@
 
-const eqArrays = function(arr1, arr2) {
-	if (arr1.length !== arr2.length) {
-		return false;
-	} else {
-		for (let i = 0; i < arr1.length; i++) {
-			if (arr1[i] !== arr2[i]) {
-				return false;
-			} else {
-			}
-		}
-	}
-	return true;
-}
-
-
-const assertArraysEqual = function(arrA, arrB) {
-	let result = eqArrays(arrA, arrB);
-	if (result === true) {
-		console.log("✅ We've got a match!")
-	} else {
-		console.log("⛔️ No match here")
-	}
-}
-
 const flatten = function(messyArr) {
    var flattenedArr = [];
    messyArr.forEach(flatten)
@@ -35,5 +11,7 @@ const flatten = function(messyArr) {
    return flattenedArr;
 }
 
-console.log(flatten([1, 2, [3, 4], 5, [6]]));
-console.log(flatten([1, {a: [2, 3]}, 4, [5, [6]], [[7], 8, 9], 10]));
+module.exports = flatten;
+
+// console.log(flatten([1, 2, [3, 4], 5, [6]]));
+// console.log(flatten([1, {a: [2, 3]}, 4, [5, [6]], [[7], 8, 9], 10]));
