6f90c1df8b1499979548d23753fd71b48ce9d845
Alyssa Beaton <ehseebe@gmail.com>
2020-06-29 20:22:28 +0000

    Removed assert functions, added export

--- without.js
+++ without.js
@@ -1,28 +1,3 @@
-
-const eqArrays = function(arr1, arr2) {
-	if (arr1.length !== arr2.length) {
-		return false;
-	} else {
-		for (let i = 0; i < arr1.length; i++) {
-			if (arr1[i] !== arr2[i]) {
-				return false;
-			} else {
-			}
-		}
-	}
-	return true;
-}
-
-
-const assertArraysEqual = function(arrA, arrB) {
-	let result = eqArrays(arrA, arrB);
-	if (result === true) {
-		console.log("✅ We've got a match!")
-	} else {
-		console.log("⛔️ No match here")
-	}
-}
-
 const without = function(source, itemsToRemove) {
 	let arrayWithout = [];
 	for (let i = 0; i < source.length; i++) {
@@ -38,12 +13,7 @@ const without = function(source, itemsToRemove) {
 	return arrayWithout;
 }
 
+module.exports = without;
 
-
-console.log(without([1, 2, 3], [1])); // => [2, 3]
-console.log(without(["1", "2", "3"], [1, 2, "3"])); 
-
-const words = ["2", "3", "3"];
-without(words, ["2"]); // no need to capture return value for this test case
-// Make sure the original array was not altered by the without function
-assertArraysEqual(words, ["2", "3", "3"]);
+// console.log(without([1, 2, 3], [1])); // => [2, 3]
+// console.log(without(["1", "2", "3"], [1, 2, "3"])); 
